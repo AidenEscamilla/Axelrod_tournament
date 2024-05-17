6df37a615462eef9a5ffc761df0caaeac4087fea
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-05-17 16:49:31 -0500

    Added bar graph

--- tournament.py
+++ tournament.py
@@ -1,12 +1,11 @@
 from player import Player
 from player import get_players
-def main():
-  number_of_turns = int(input("Enter the number of turns per round: "))
+import matplotlib.pyplot as plt
 
-  players_1 = get_players(number_of_turns)
-  players_2 = get_players(number_of_turns)
-  for player1 in players_1: # Only keep player 1's score but both are about equal
-    for player2 in players_2:
+def run_simulation(number_of_turns, main_players, opponent_players):
+
+  for player1 in main_players: # Only keep player 1's score but both are about equal
+    for player2 in opponent_players:
       print(f"{player1.name} vs {player2.name}")
       # Play rounds against each other
       for _ in range(number_of_turns):
@@ -17,10 +16,33 @@ def main():
       # Reset memory and iterator for next opponent
       player2.reset()
       player1.reset()
+
+def display_graph(names, scores):
+  fig, ax = plt.subplots(figsize=(15, 10))
+  bar_container = ax.bar(names, scores)
+  ax.set(ylabel='Total points scored', title='LongView Prisoners Dilemma Tournament')
+  ax.bar_label(bar_container, scores)
+  plt.show()
+
+
+def main():
+  number_of_turns = int(input("Enter the number of turns per round: "))
+
+  players_1 = get_players(number_of_turns)
+  players_2 = get_players(number_of_turns)
+  run_simulation(number_of_turns, players_1, players_2)
   
   for player in players_1:
     print(f"{player.name}: {player.total_score}")
 
+  names_list = []
+  score_list = []
+  for player in players_1:
+    names_list.append(player.name)
+    score_list.append(player.total_score)
+
+  display_graph(names_list, score_list)
+  
     
 
 if __name__ == '__main__':
