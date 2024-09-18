dad01b764bc39b00928c788fd544a71464280e7c
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-09-18 11:45:03 -0500

    Added all class 3 strats

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ import random
 import time
 
 class Player:
-  def __init__(self, name, strategy, chance_array=None):
+  def __init__(self, name, strategy, chance_array=None, num_rounds=None):
     self.opponent_memory = []
     self.self_memory = []
     self.name = name
@@ -13,6 +13,9 @@ class Player:
     self.chance_iter = 0
     self.temp_strat = None
     self.opponent_score = 0
+    self.total_num_rounds = num_rounds  # This is cheating, the argument should be deleted as well as this variable
+    # Including the number of rounds is to appease the tricky_tft strat group when I pull the rug out
+    # And say "What if we didn't do just 100 rounds"
 
   def __str__(self):
     return f"{self.name} scored: {self.total_score}"
@@ -132,7 +135,7 @@ class Player:
     output_array = np.array(output_array)
     np.random.shuffle(output_array)
 
-    return output_array
+    return output_array.tolist()
   
   '''
   IMPORTANT: This does not require 0's and 1's. The array could just be 'defect' or 'cooperate'.
@@ -502,16 +505,149 @@ def two_chances(player):
 def grass_touchers(player):
   return tit_for_tat(player)
 
+# Class 3
+
+def dominators(player):
+  self_memory = player.get_self_memory()
+    # player.chance_iter == 4 if it's been 4 rounds
+    # (len(self_mem) // 4) % 3 = every 4 rounds we switch
+  if player.chance_iter == 4 and (len(self_memory) // 4) % 3 == 0:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(80, 4)) # switch to 80% defect
+  elif player.chance_iter == 4 and (len(self_memory) // 4) % 3 == 1:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_choice_pattern_array([1,1,0,0], 4))  # switch to 50% defect
+  elif player.chance_iter == 4 and (len(self_memory) // 4) % 3 == 2:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(100, 4))  # switch to 100% defect
+  
+
+
+  bool_choice = player.get_next_array_element() # Get the choice
+  if bool_choice: # 1 == defect
+    return "defect"
+  else: # 0 == cooperate
+    return "cooperate" 
+
+def meaner_tft(player):
+  opponent_memory = player.get_opponent_memory()
+
+  # For round 1 or round 2
+  if len(opponent_memory) == 0 or len(opponent_memory) == 1:
+    return "defect"
+  
+  return opponent_memory[-1] # Copy what they did last turn
+
+def mean_tft(player):
+  opponent_memory = player.get_opponent_memory()
+
+  # For round 1 or round 2
+  if len(opponent_memory) == 0:
+    return "defect"
+  else:
+    return opponent_memory[-1] # Copy what they did last turn
+
+def swap_random(player):
+  self_memory = player.get_self_memory()
+    # player.chance_iter == 5 if it's been 5 rounds
+    # (len(self_mem) // 5) % 5 = every 5 rounds we switch strats
+  if player.chance_iter == 5 and (len(self_memory) // 5) % 5 == 0:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(40, 5)) # switch to 40% defect
+  elif player.chance_iter == 5 and (len(self_memory) // 5) % 5 == 1:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(100, 5))  # switch to 100% defect
+  elif player.chance_iter == 5 and (len(self_memory) // 5) % 5 == 2:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(70, 5))  # switch to 70% defect
+  elif player.chance_iter == 5 and (len(self_memory) // 5) % 5 == 3:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(20, 5))  # switch to 20% defect
+  elif player.chance_iter == 5 and (len(self_memory) // 5) % 5 == 4:
+    player.reset_iterator()
+    player.set_chance_array(Player.get_chance_array(65, 5))  # switch to 65% defect
+  
+
+
+  bool_choice = player.get_next_array_element() # Get the choice
+  if bool_choice: # 1 == defect
+    return "defect"
+  else: # 0 == cooperate
+    return "cooperate" 
+
+def special_case(player):
+  opponent_memory = player.get_opponent_memory()
+  if len(opponent_memory) <= 92:  # For the first 92 turns
+    return "defect"
+  
+  if len(opponent_memory) >= 93: # On turn 93 and beyond
+    opp_choice_set = set(opponent_memory[-93:]) # Make a set of their last 93 choices
+
+    # Set can ONLY equal = {"coop"} OR {"defect"} OR {"coop","defect"}
+
+    # If they cooperated for the last 93 rounds    
+    if len(opp_choice_set) == 1 and (opp_choice_set.pop() == "cooperate"):
+      return "cooperate"  # be nice
+    else:
+      return "defect" # be mean
+
+def tricky_tft(player):
+  opponent_memory = player.get_opponent_memory()
+
+  if len(opponent_memory) == 0: # On turn 1
+    return "cooperate"  # be nice
+  elif len(opponent_memory) == (player.total_num_rounds - 1): # the last turn
+    return "defect" # be mean
+  else: # All other turns
+    return opponent_memory[-1] 
+  # Do what the opponent did last round
+
+def pattern_recognition(player):
+  opponent_memory = player.get_opponent_memory()
+
+  # On turn 1 or 3
+  if len(opponent_memory) == 0 or len(opponent_memory) == 2:
+    return "defect"
+  elif len(opponent_memory) == 1: # On turn 2
+    return "cooperate"
+  
+  opponent_pattern = opponent_memory[0:3] # Get their first 3 moves
+  pattern_recognized = None
+
+  if opponent_pattern == ["cooperate", "defect", "cooperate"]:
+    pattern_recognized = "tit_for_tat"
+  elif opponent_pattern == ["defect", "defect", "defect"]:
+    pattern_recognized = "devil"
+  elif opponent_pattern == ["defect", "cooperate", "defect"]:
+    pattern_recognized = "self" # and inverse_tft
+  elif opponent_pattern == ["cooperate","cooperate","cooperate"]:
+    pattern_recognized = "Mr.Moore"
+
+  if pattern_recognized == "Mr.Moore":
+    player.set_temp_strat(bad_guy)
+  elif pattern_recognized == "devil" or pattern_recognized == "tit_for_tat":
+    player.set_temp_strat(tit_for_tat)
+  elif pattern_recognized == "self": # Or inverse_tft
+    player.set_temp_strat(inverse_tit_for_tat)
+  else:
+    player.set_temp_strat(tit_for_tat)
+
+  return player.get_temp_strat_result()
+
 
 #Add players here when adding them to the tournament 
 def get_players(number_of_turns):
   players = []
+  # Bots
+  players.append(Player('50/50', random_or_preset_choices, Player.get_chance_array(50, number_of_turns)))
+  players.append(Player('Mr.Moore', good_guy))
+  players.append(Player('inverse_tft', inverse_tit_for_tat))
+  players.append(Player('tit_for_tat', tit_for_tat))
+  players.append(Player('devil', bad_guy))
+
   # Class 1
   # players.append(Player('Name_here1', recognize))
-  # players.append(Player('50/50', random_or_preset_choices, Player.get_chance_array(50, number_of_turns)))
-  # players.append(Player('Mr.Moore', good_guy))
   # players.append(Player('DaSupaStrat', super_strat))
-  # players.append(Player('inverse_tft', inverse_tit_for_tat))
   # players.append(Player('hoi4', heart_of_iron_4))
   # players.append(Player('Name_here2', random_or_preset_choices, Player.get_choice_pattern_array( [1, 1, 0, 1, 1, 1, 0, 1, 1, 1] ,number_of_turns)))
   # players.append(Player('Bob', random_or_preset_choices, Player.get_choice_pattern_array([1, 0, 1, 1, 1, 1, 0, 1, 1, 1], number_of_turns)))
@@ -519,25 +655,31 @@ def get_players(number_of_turns):
   # players.append(Player('kaynes', random_or_preset_choices, Player.get_chance_array(75 ,number_of_turns)))
   # players.append(Player('Jeff', bad_guy))
   # players.append(Player('Cool_beans', cool_beans))
-  # players.append(Player('tit_for_tat', tit_for_tat))
   # players.append(Player('DaSupaStrat', super_strat))
   # players.append(Player('Computer Goose', computer_goose))
 
   # Class 2
-  players.append(Player('50/50', random_or_preset_choices, Player.get_chance_array(50, number_of_turns)))
-  players.append(Player('Mr.Moore', good_guy))
-  players.append(Player('inverse_tft', inverse_tit_for_tat))
-  players.append(Player('tit_for_tat', tit_for_tat))
-  players.append(Player('devil', bad_guy))
-  players.append(Player('semi_random', semi_random))
-  players.append(Player('demo_dolphin', be_nice_twice))
-  players.append(Player('team_Elo', random_or_preset_choices, Player.get_choice_pattern_array([1, 1, 1, 1, 0, 1, 1, 0], number_of_turns)))
-  players.append(Player('glass_saturn', semi_random_2))
-  players.append(Player('vroom', random_or_preset_choices, Player.get_choice_pattern_array([1, 1, 1, 1, 1, 0], number_of_turns)))
-  players.append(Player('carlson_method', two_chances))
-  players.append(Player('wipers', random_or_preset_choices, Player.get_chance_array(40 ,number_of_turns)))
-  players.append(Player('grass_touchers', grass_touchers))
-  players.append(Player('pickles', random_or_preset_choices, Player.get_choice_pattern_array([0,1,1,0,0,1,1,0,1,1],number_of_turns)))
+  # players.append(Player('addie_abby', semi_random))
+  # players.append(Player('demo_dolphin', be_nice_twice))
+  # players.append(Player('team_Elo', random_or_preset_choices, Player.get_choice_pattern_array([1, 1, 1, 1, 0, 1, 1, 0], number_of_turns)))
+  # players.append(Player('glass_saturn', semi_random_2))
+  # players.append(Player('vroom', random_or_preset_choices, Player.get_choice_pattern_array([1, 1, 1, 1, 1, 0], number_of_turns)))
+  # players.append(Player('carlson_method', two_chances))
+  # players.append(Player('wipers', random_or_preset_choices, Player.get_chance_array(40 ,number_of_turns)))
+  # players.append(Player('grass_touchers', grass_touchers))
+  # players.append(Player('pickles', random_or_preset_choices, Player.get_choice_pattern_array([0,1,1,0,0,1,1,0,1,1],number_of_turns)))
+
+  # Class 3
+  players.append(Player('dominators', dominators, Player.get_chance_array(80,4)))
+  players.append(Player('serpent', meaner_tft))
+  players.append(Player("Aztec_em", swap_random, Player.get_chance_array(40,5)))
+  players.append(Player('Gold_qs', mean_tft))
+  players.append(Player('name_here', bad_guy))
+  players.append(Player('Lemon', random_or_preset_choices, Player.get_chance_array(70, number_of_turns)))
+  players.append(Player('op_code', special_case))
+  players.append(Player('pg13', tricky_tft, None, number_of_turns))
+  players.append(Player('we_rock_inc.', random_or_preset_choices, Player.get_chance_array(70, number_of_turns)))
+  players.append(Player('bananas', pattern_recognition))
   return players
   
 
